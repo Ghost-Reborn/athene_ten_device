883cc5e481345a180f156c5044c1574c9379ff2b
Erfan Abdi <erfangplus@gmail.com>
2019-09-09 02:23:14 +0430

    athene: Build AOSP camera provider service
    * But use moto camera.device@1.0-impl.so
    
    Change-Id: I8a907de60f9ae4c903740282253b91fe8e5bc273
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -16,7 +16,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-impl:32
 
 # Crypto
 PRODUCT_PACKAGES += \
