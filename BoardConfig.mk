934cce6e0309381db4a4e13b26681bd6103f3409
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2021-08-06 22:46:49 +0530

    athene: SEPolicy fix * Nuke data-ipa-cfg-mgr * Added charder animation

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -250,7 +250,7 @@ VENDOR_SECURITY_PATCH := 2018-12-01
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/legacy
 
--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,18 +17,13 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#pragma push_macro("PROPERTY_VALUE_MAX")
-
-#include <cutils/properties.h>
-#include <string.h>
-
-#define BTM_DEF_LOCAL_NAME "Moto G4"
+#define BTM_DEF_LOCAL_NAME "Moto G4 Plus"
 #define BLUETOOTH_QTI_SW TRUE
-#define MAX_ACL_CONNECTIONS   7
+#define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
+#define BLE_VND_INCLUDED TRUE
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
-
-#pragma pop_macro("PROPERTY_VALUE_MAX")
-
 #endif
