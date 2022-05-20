f5888b3d79109e1b1df0dd622afe64cf1e3ee761
TheGhostReborn <abhinandammanamkandy2001@gmail.com>
2022-05-20 12:29:15 +0530

    athene: Don't remove any packages

--- remove-packages/Android.mk
+++ remove-packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Camera2 Dialer Contacts messaging Snap ExactCalculator DeskClock
+LOCAL_OVERRIDES_PACKAGES := 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
